d4910bb2a8ffb30bb11380c41d4f47b7da1a59eb
LALIT JAGTAP <jagtap_lalit@yahoo.com>
2018-06-29 08:18:32 -0500

    Initial Commit

---
+++ SwiftyWords/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyWords
+//
+//  Created by LALIT JAGTAP on 6/29/18.
+//  Copyright © 2018 LALIT JAGTAP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
